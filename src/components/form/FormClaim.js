97b02f295543d57d81a7c8880e33d59a1209c714
Pierre Baele <pierre.baele-prestataire@laposte.fr>
2018-04-18 16:01:17 +0200

    added more components to form

--- src/components/form/FormClaim.js
+++ src/components/form/FormClaim.js
@@ -1,46 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FormControl, FormControlLabel, Radio, RadioGroup, withStyles} from 'material-ui';
+import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, withStyles} from 'material-ui';
+import Button from "material-ui/es/Button/Button";
 
 const styles = theme => ({
     root: {
-        display: 'flex',
+        textAlign: 'center',
     },
     formControl: {
         margin: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit,
+        padding: 0,
+
     },
-    group: {
-        margin: `${theme.spacing.unit}px 0`,
+    radioGroup: {
+        display: 'flex',
+        flexDirection: 'row',
     },
+    nameInput: {
+      // marginLeft: theme.spacing.unit * 3,
+    },
+    submitButton: {
+        margin: theme.spacing.unit,
+        position: 'relative',
+    }
 });
 
 
 class FormClaim extends React.Component {
 
     state = {
-        value: 'Femme',
+        genre: 'male',
     };
 
-    handleChange = event => {
-        this.setState({ value: event.target.value });
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
     };
 
     render() {
         const { classes } = this.props;
 
         return(
-        <div>
-            <FormControl component="fieldset" required className={classes.formControl} >
+        <div className={classes.root}>
+            <form className={classes.containerForm}>
+            <FormControl component="fieldset" required className={classes.formControl}>
+                <FormLabel component="legend">Genre</FormLabel>
+
                 <RadioGroup
-                    aria-label="gender"
-                    name="gender2"
-                    value={this.state.value}
-                    onChange={this.handleChange}
+                    value={this.state.genre}
+                    onChange={this.handleChange('genre')}
+                    className={classes.radioGroup}
                 >
                 <FormControlLabel value="male" control={<Radio color="primary" />} label="Homme" />
                 <FormControlLabel value="female" control={<Radio color="primary" />} label="Femme" />
                 </RadioGroup>
+l
+                <div>
+                <span style={{width: '50px',display:'inline-block'}}>{this.state.genre === 'male'? 'Mr': 'Mme'}</span>
+                <TextField
+                    id="name"
+                    label="Nom Prénom"
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    className={classes.nameInput}
+                />
+                </div>
+
             </FormControl>
+                <div>
+                <Button variant="raised" color="primary" className={classes.submitButton}>
+                    Valider
+                </Button>
+                </div>
+
+            </form>
+
         </div>
             )
     }
