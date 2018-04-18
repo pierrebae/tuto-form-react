005dce641f6c53b231b5ad101976fb172bd62826
Pierre Baele <pierre.baele-prestataire@laposte.fr>
2018-04-18 18:06:37 +0200

    added select input

--- src/components/form/FormClaim.js
+++ src/components/form/FormClaim.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, withStyles} from 'material-ui';
 import Button from "material-ui/es/Button/Button";
+import Select from "material-ui/es/Select/Select";
+import MenuItem from "material-ui/es/Menu/MenuItem";
+import InputLabel from "material-ui/es/Input/InputLabel";
 
 const styles = theme => ({
     root: {
@@ -10,20 +13,31 @@ const styles = theme => ({
     formControl: {
         margin: theme.spacing.unit * 3,
         marginBottom: theme.spacing.unit,
-        padding: 0,
+        padding: 50,
+        minWidth: 120,
 
     },
     radioGroup: {
         display: 'flex',
         flexDirection: 'row',
+        // alignItems: 'center',
+
+
+    },
+    radioInput: {
+        // display:'inline-block',
     },
     nameInput: {
       // marginLeft: theme.spacing.unit * 3,
+    },
+    selectInput: {
+        marginTop: theme.spacing.unit * 3,
     },
     submitButton: {
         margin: theme.spacing.unit,
         position: 'relative',
     }
+
 });
 
 
@@ -31,8 +45,11 @@ class FormClaim extends React.Component {
 
     state = {
         genre: 'male',
+        motif: 'AUTRE'
     };
 
+    //currying function function qui prends name qui retourne une fonction qui prend l'event en argument et qui execute une fonction à partir de ces fonctions
+    // translating the evaluation of a function that takes multiple arguments (or a tuple of arguments) into evaluating a sequence of functions, each with a single argument.
     handleChange = name => event => {
         this.setState({
             [name]: event.target.value,
@@ -46,17 +63,16 @@ class FormClaim extends React.Component {
         <div className={classes.root}>
             <form className={classes.containerForm}>
             <FormControl component="fieldset" required className={classes.formControl}>
-                <FormLabel component="legend">Genre</FormLabel>
-
                 <RadioGroup
                     value={this.state.genre}
                     onChange={this.handleChange('genre')}
                     className={classes.radioGroup}
+                    row={true}
                 >
-                <FormControlLabel value="male" control={<Radio color="primary" />} label="Homme" />
-                <FormControlLabel value="female" control={<Radio color="primary" />} label="Femme" />
+                    <FormControlLabel classes={classes.radioInput} value="male" control={<Radio color="primary" />} label="Homme" />
+                    <FormControlLabel classes={classes.radioInput} value="female" control={<Radio color="primary" />} label="Femme" />
                 </RadioGroup>
-l
+
                 <div>
                 <span style={{width: '50px',display:'inline-block'}}>{this.state.genre === 'male'? 'Mr': 'Mme'}</span>
                 <TextField
@@ -67,7 +83,23 @@ l
                     className={classes.nameInput}
                 />
                 </div>
-
+                <div>
+                    <InputLabel htmlFor="motif">Motif</InputLabel>
+                    <Select
+                    value={this.state.motif}
+                    onChange={this.handleChange}
+                    className={classes.selectInput}
+                    inputProps={{
+                        name: 'motif',
+                        id: 'motif',
+                    }}
+                    >
+                    <MenuItem value={'AUTRE'}>Autre</MenuItem>
+                    <MenuItem value={'MOTIF1'}>Motif 1</MenuItem>
+                    <MenuItem value={'MOTIF2'}>Motif 2</MenuItem>
+                    <MenuItem value={'MOTIF3'}>Motif 3</MenuItem>
+                </Select>
+                </div>
             </FormControl>
                 <div>
                 <Button variant="raised" color="primary" className={classes.submitButton}>
